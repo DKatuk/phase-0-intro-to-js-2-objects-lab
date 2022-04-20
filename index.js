e1916ba9b79e76e1156c353e6b7106d7316b5506
Dilara Katuk <dilarakatuk@gmail.com>
2022-04-20 23:54:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let employee = {
+    name : "Dilara",
+    streetAddress: 94
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employees= { ...employee};
+    employees[key] = value;
+    return employees;
+}
+updateEmployeeWithKeyAndValue(employee, "name", "Sam");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key,value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employee1= { ...employee};
+    delete employee1[key];
+    return employee1;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
